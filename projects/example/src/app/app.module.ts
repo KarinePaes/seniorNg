a07b88ce595bf1da109bc2a420d01fbcd3cc9ad1
thallis-andre <thallis-andre@hotmail.com>
2023-06-21 19:06:02 -0300

    chore: add angular material

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextAdaptationModule } from 'projects/senior-ng/src/lib/text-adaptation/text-adaptation.module';
+import { HighlightLinkModule } from 'projects/senior-ng/src/lib/highlight-link/highlight-link.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TextAdaptationModule } from 'projects/senior-ng/src/lib/text-adaptation
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TextAdaptationModule
+    TextAdaptationModule,
+    HighlightLinkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/senior-ng/src/lib/highlight-link/highlight-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sng-highlight-link',
+  templateUrl: './highlight-link.component.html',
+  styleUrls: ['./highlight-link.component.scss']
+})
+export class HighlightLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
