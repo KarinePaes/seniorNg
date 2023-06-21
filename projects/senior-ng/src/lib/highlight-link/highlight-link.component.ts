cc444caac78b0a555935069094732a7e7e6b8815
thallis-andre <thallis-andre@hotmail.com>
2023-06-21 20:35:01 -0300

    feat: criação do componente highlight link

--- projects/senior-ng/src/lib/highlight-link/highlight-link.component.ts
+++ projects/senior-ng/src/lib/highlight-link/highlight-link.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sng-highlight-link',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./highlight-link.component.scss']
 })
 export class HighlightLinkComponent implements OnInit {
+
+  @Input() link: string = '';
+  @Input() textLink: string = '';
+  @Input() textTooltip: string = '';
+  @Input() ariaLabel: string = '';
 
   constructor() { }
 
