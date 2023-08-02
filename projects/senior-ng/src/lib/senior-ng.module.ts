3437ceb73fd2987b0969c0bfa3b2c3d3153dbf0f
thallis-andre <thallis-andre@hotmail.com>
2023-08-02 19:05:09 -0300

    fix: removendo diretiva inutilizada

--- projects/senior-ng/src/lib/senior-ng.module.ts
+++ projects/senior-ng/src/lib/senior-ng.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { MouseIncreaseDirective } from '../directives/mouse-increase.directive';
 
 import { TextAdaptationModule } from './text-adaptation/text-adaptation.module';
 
@@ -7,13 +6,11 @@ import { TextAdaptationModule } from './text-adaptation/text-adaptation.module';
 
 @NgModule({
   declarations: [
-    MouseIncreaseDirective,
   ],
   imports: [
     TextAdaptationModule,
   ],
   exports: [
-    MouseIncreaseDirective
   ]
 })
 export class SeniorNgModule { }
