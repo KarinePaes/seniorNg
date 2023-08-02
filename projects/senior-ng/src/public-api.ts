3bce56dfda55a207380f1a4723aed1b0de238348
thallis-andre <thallis-andre@hotmail.com>
2023-08-02 19:33:50 -0300

    feat: exportando no public-api

--- projects/senior-ng/src/public-api.ts
+++ projects/senior-ng/src/public-api.ts
@@ -2,5 +2,11 @@
  * Public API Surface of senior-ng
  */
 
-export * from './lib/mouse-increase/mouse-increase.component';
+export * from './lib/mouse-increase/mouse-increase.module';
+export * from './lib/action-warning/action-warning.module';
+export * from './lib/dropdownToList/dropdownToList.module';
+export * from './lib/highlight-link/highlight-link.module';
+export * from './lib/hint/hint.module';
+export * from './lib/text-adaptation/text-adaptation.module';
+export * from './directives/directives.module';
 export * from './lib/senior-ng.module';
